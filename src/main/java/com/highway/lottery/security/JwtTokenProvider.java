abbe69adc9b07adc40e3048034c076556560b270
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-05-08 22:36:36 +0800

    implemented Jwt Auth

---
+++ src/main/java/com/highway/lottery/security/JwtTokenProvider.java
@@ -0,0 +1,72 @@
+package com.highway.lottery.security;
+import com.highway.lottery.exception.APIException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secrete}")
+    private String secrete;
+    @Value("${app.jwt-issuer}")
+    private String issuer;
+    @Value("${app.jwt-expiration-milliseconds}")
+    private long jwtExpiration;
+
+    // generate token
+    public String generateToken(Authentication authentication) {
+         var username = authentication.getName();
+         var currentDate = new Date();
+         var expirationDate = new Date(currentDate.getTime() + jwtExpiration);
+         String token = Jwts.builder()
+                 .setSubject(username)
+                 .setIssuer(issuer)
+                 .setIssuedAt(currentDate)
+                 .setExpiration(expirationDate)
+                 .signWith(key())
+                 .compact();
+        return token;
+    }
+
+    // get username from jwtToken
+    public String getUsernameFromToken(String jwtToken) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(jwtToken).getBody();
+        return claims.getSubject();
+    }
+    // validate jwtToken
+    public boolean validateToken(String jwtToken){
+        try{
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJws(jwtToken).getBody();
+            return !claims.getExpiration().before(new Date());
+        }
+         catch (ExpiredJwtException e) {
+            throw  new APIException(HttpStatus.BAD_REQUEST,"Token expired: " + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            throw  new   APIException(HttpStatus.BAD_REQUEST,"Unsupported JWT: " + e.getMessage());
+        } catch (MalformedJwtException e) {
+            System.out.println("Malformed JWT: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            throw  new   APIException(HttpStatus.BAD_REQUEST,"Illegal argument: " + e.getMessage());
+        }
+        return false;
+    }
+
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secrete));
+    }
+}
---
+++ src/main/java/com/highway/lottery/security/SecurityUser.java
@@ -0,0 +1,53 @@
+package com.highway.lottery.security;
+import com.highway.lottery.domain.entity.Account;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class SecurityUser implements UserDetails {
+    private final Account account;
+
+    public SecurityUser(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.account.getRoles().
+                stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return account.isEnabled();
+    }
+}
